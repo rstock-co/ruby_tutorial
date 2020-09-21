12d31a366f9eefbcfdae5bcb3acfa63a14b28c8e
Richard Stock <rstock.co@gmail.com>
2020-09-21 13:05:42 -0600

    exercise 4.5 count.rb

--- hello_app.rb
+++ hello_app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
     get '/' do
-      'hello world!'
+      DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+      dayname = DAYNAMES[Time.now.wday]
+      "hello world! Happy #{dayname}"
     end
