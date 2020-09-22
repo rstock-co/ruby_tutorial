b5a62cfd24bd4b664cf595150af085ec1566c6a1
Richard Stock <rstock.co@gmail.com>
2020-09-22 06:02:09 -0600

    modified palindrome.rb per Chapter 7 exercises

--- palindrome.rb
+++ palindrome.rb
@@ -1,6 +1,27 @@
-# Returns true for a palindrome, false otherwise.
 
-def palindrome?(string)
-  processed_content = string.downcase
-  processed_content == string.downcase.reverse 
+# Defines a Phrase class.
+class Phrase < String
+
+  # Returns content for palindrom testing.
+  def processed_content
+    self.downcase
+  end
+
+  # Returns true for palindrome
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
 end
+
+# Defines a translated Phrase.
+class TranslatedPhrase < Phrase
+  attr_accessor :translation
+
+  def initialize(content, translation)
+    super(content)
+    @translation = translation
+  end
+end
+
+TranslatedPhrase.new("recognize","reconocer")
+
