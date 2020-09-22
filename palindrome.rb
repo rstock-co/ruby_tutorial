11c2186ef5b2a65518a7e723193e9f6da09c1263
Richard Stock <rstock.co@gmail.com>
2020-09-22 09:55:00 -0600

    modified palindrome.rb to make module

--- palindrome.rb
+++ palindrome.rb
@@ -1,27 +1,19 @@
-
-# Defines a Phrase class.
-class Phrase < String
-
-  # Returns content for palindrom testing.
-  def processed_content
-    self.downcase
-  end
+module Palindrome
 
   # Returns true for palindrome
   def palindrome?
     processed_content == processed_content.reverse
   end
-end
 
-# Defines a translated Phrase.
-class TranslatedPhrase < Phrase
-  attr_accessor :translation
+  private
 
-  def initialize(content, translation)
-    super(content)
-    @translation = translation
+    # Returns content for palindrom testing.
+    def processed_content
+      self.downcase
+    end
+end  
+
+  class String
+    include Palindrome
   end
-end
-
-TranslatedPhrase.new("recognize","reconocer")
-
+ 
